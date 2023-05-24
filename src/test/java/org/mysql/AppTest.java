70252d759b7f260868eb3d528e1e186ed01f625d
Dkid555 <dimason2008@gmail.com>
2023-05-24 16:37:46 -0400

    Maven MySql local DB

---
+++ src/test/java/org/mysql/AppTest.java
@@ -0,0 +1,32 @@
+package org.mysql;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    public static boolean CsvTest()
+    {
+//        String path = "C:\Users\dima\TEST\test.csv";
+        return true;
+    }
+
+
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
